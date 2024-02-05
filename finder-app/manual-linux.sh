73f04a56d04a1fa303b39234d6a305163c2506e4
akash <akash@ubuntu20.4.myguest.virtualbox.org>
2024-02-05 12:18:27 -0700

    changes

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -150,7 +150,7 @@ sudo chown -R root:root *
 
 # TODO: Create initramfs.cpio.gz
 cd "$OUTDIR/rootfs"
-find. | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
 echo "here"
 cd ${OUTDIR}
 gzip -f initramfs.cpio
