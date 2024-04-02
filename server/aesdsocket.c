f90d4649196b42643a5171497d9c32e5f303a02f
akka2103 <143990610+akka2103@users.noreply.github.com>
2024-04-02 14:59:02 -0600

    Update aesdsocket.c

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -228,11 +228,11 @@ void handle_client_connection(int client_fd)
          // Check if the received command is AESDCHAR_IOCSEEKTO:X,Y
     if (strncmp(bptr, "AESDCHAR_IOCSEEKTO:", 19) == 0)
     {
-        unsigned int x, y;
-        if (sscanf(bptr + 19, "%u,%u", &x, &y) == 2)
+	struct aesd_seekto temp_seek;
+        if (sscanf(bptr + 19, "%u,%u", &temp_seek.write_cmd, &temp_seek.write_cmd_offset) == 2)
         {
             // Perform ioctl operation with X and Y values
-            if (ioctl(device_fd, AESDCHAR_IOCSEEKTO, (unsigned long)((uint64_t)x << 32 | y)) == -1)
+            if (ioctl(device_fd, AESDCHAR_IOCSEEKTO, &temp_seek) == -1)
             {
                 syslog(LOG_ERR, "Error performing ioctl operation: %m");
                 free(bptr);
